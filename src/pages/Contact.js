e7da5814c11e803cd8bc827ae520092b08761c10
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-07-19 18:25:55 -0400

    added contact page elements

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,14 @@
 import React from "react";
+import plane from "../assets/contact-plane-vector.png"
 
 function Contact() {
   return (
     <div className="body-container"> 
       <h1 className="page-title">Contact</h1>
       <h1>I would love to hear from you!</h1>
+      <img src={plane} alt='paper plane with dotted line trail'/>
+      <p>In lieu of a carrier pigeon or a paper airplane, I can be reached by email.</p>
+      <p>kyrstin.kempf@gmail.com</p>
     </div>
   );
 }
