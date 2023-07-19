3303adaeb543c935908c1b77cb30d60d7d43c22e
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-07-19 18:19:05 -0400

    add home page elements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import profile from '../images/HeadshotFull.jpeg'
+import profileBw from '../images/profile-bw.jpeg'
 
 function Home() {
   return (
-    <div> 
-        <h1>Home</h1>
+    <div className="body-container"> 
+        <h1 id="home-intro">Hi, I’m Kyrstin Kempf.</h1>
+        <p className="intro-p">I’m a full-stack software engineer based in Washington, DC. Welcome to my little corner of the cyber world.</p>
+        <p className="intro-p">What makes me passionate about software engineering is the power to design the organization of information and how users interact with it.</p>
+        {/* <img id="profile-img" src={profile} alt='Kyrstin'/> */}
+        <img id="profile-img" src={profileBw} alt='Kyrstin'/>
     </div>
   );
 }
