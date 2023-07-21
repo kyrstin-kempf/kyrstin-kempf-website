9d383646ac98dc74770d6ccb69485cc1d75288ab
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-07-21 17:44:42 -0400

    add child divs for inline-block design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,16 @@ import profileBw from '../images/profile-bw.jpeg'
 function Home() {
   return (
     <div className="body-container"> 
-        <h1 id="home-intro">Hi, I’m Kyrstin Kempf.</h1>
-        <p className="intro-p">I’m a full-stack software engineer based in Washington, DC. Welcome to my little corner of the cyber world.</p>
-        <p className="intro-p">What makes me passionate about software engineering is the power to design the organization of information and how users interact with it.</p>
-        {/* <img id="profile-img" src={profile} alt='Kyrstin'/> */}
-        <img id="profile-img" src={profileBw} alt='Kyrstin'/>
+      <div className="home-parent-block">
+        <div className="home-child-block" id="one">
+          <h1 id="home-intro">Hi, I’m Kyrstin Kempf.</h1>
+          <p className="intro-p">I’m a full-stack software engineer based in Washington, DC. Welcome to my little corner of the cyber world.</p>
+          <p className="intro-p">What makes me passionate about software engineering is the power to design the organization of information and how users interact with it.</p>
+        </div>
+        <div className="home-child-block" id="two">
+          <img id="profile-img" src={profileBw} alt='Kyrstin'/>
+        </div>
+      </div>
     </div>
   );
 }
