0e940ed15dcdcd62a81504675265fef2a356558d
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-07-27 14:37:02 -0400

    added link to individual project page to each div

--- src/OnePortfolioTile.js
+++ src/OnePortfolioTile.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 function OnePortfolioTile({ project }) {
   return (
-    <div>
-      <div className="portfolio-title">
-        <h1>{project.title}</h1>
+    <Link to={`/projects/${project.id}`} className="project-tile">
+      <div>
+        <div className="portfolio-title">
+          <h1>{project.title}</h1>
+        </div>
+        <div className="portfolio-image">
+          <img src={project.tile_image} alt="" />
+        </div>
       </div>
-      <div className="portfolio-image">
-        <img src={project.tile_image} alt="" />
-      </div>
-    </div>
+    </Link>
   );
 }
 export default OnePortfolioTile;
