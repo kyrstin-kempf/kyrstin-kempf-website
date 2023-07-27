cff0868767271daa83ca327bd08ebeff37dd6978
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-07-27 12:14:38 -0400

    created portfolio tile component

---
+++ src/OnePortfolioTile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function OnePortfolioTile() {
+  return (
+    <div className="tile">
+      <div className="portfolio-title">
+        <h1>Title</h1>
+      </div>
+      <div className="portfolio-image">
+        <p>test</p>
+      </div>
+    </div>
+  );
+}
+export default OnePortfolioTile;
