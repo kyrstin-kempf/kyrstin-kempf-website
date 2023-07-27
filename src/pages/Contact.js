8fec27bf0a93a32780969c64cf7ac52b5e633878
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-07-27 15:57:12 -0400

    add classnames

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,10 +5,12 @@ function Contact() {
   return (
     <div className="body-container"> 
       <h1 className="page-title">Contact</h1>
-      <h1>I would love to hear from you!</h1>
-      <img src={plane} alt='paper plane with dotted line trail'/>
-      <p>In lieu of a carrier pigeon or a paper airplane, I can be reached by email.</p>
-      <p>kyrstin.kempf@gmail.com</p>
+      <h2 className="contact-h2">I would love to hear from you!</h2>
+      <img className="contact-plane" src={plane} alt='paper plane with dotted line trail'/>
+      <div className="contact-p">
+        <p>In lieu of a carrier pigeon or a paper airplane, I can be reached by email.</p>
+        <p>kyrstin.kempf@gmail.com</p>
+      </div>
     </div>
   );
 }
