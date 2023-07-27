1cdc10d91e5a5746faae15b754d0eacb8cccd25c
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-07-27 13:15:42 -0400

    fetched projects in db.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import NavBar from "./NavBar";
@@ -9,15 +9,27 @@ import Contact from "./pages/Contact";
 import OneProject from "./pages/OneProject";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect( () => {
+    const fetchProjects = async () => {
+      const response = await fetch('http://localhost:3001/projects')
+      const data = await response.json();
+      setProjects(data);
+    }
+
+    fetchProjects();
+  }, []);
+
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/about" element={ <About /> } />
-        <Route path="/portfolio" element={ <Portfolio /> } />
+        <Route path="/portfolio" element={ <Portfolio projects={projects} /> } />
         <Route path="/contact" element={ <Contact /> } />
-        <Route path="/portfolio/:id" element={ <OneProject /> } />
+        <Route path="/portfolio/:id" element={ <OneProject projects={projects} /> } />
       </Routes>
     </BrowserRouter>
   );
