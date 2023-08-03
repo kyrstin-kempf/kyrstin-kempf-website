82df150b52b0cf307ea99df809cdf7cfd31429cd
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-08-03 00:51:26 -0400

    updated home content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,12 @@ function Home() {
       <div className="home-parent-block">
         <div className="home-child-block" id="one">
           <h1 id="home-intro">Hi, I’m Kyrstin Kempf.</h1>
-          <p className="intro-p">I’m a full-stack software engineer based in Washington, DC. Welcome to my little corner of the cyber world.</p>
-          <p className="intro-p">What makes me passionate about software engineering is the power to design the organization of information and how users interact with it.</p>
+          <p className="intro-p">
+            I’m a full-stack software engineer based in Washington, D.C. Welcome to my little corner of the cyber world.
+          </p>
+          <p className="intro-p">
+          Software engineering allows me to pursue my passion for creating intentional spaces - simple, organized spaces that that maximize function without losing aesthetic value.
+          </p>
         </div>
         <div className="home-child-block" id="two">
           <img id="profile-img" src={profileBw} alt='Kyrstin'/>
