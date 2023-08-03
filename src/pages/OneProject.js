2465baf34ae1901a7f460218eb7a8f49ca832486
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-08-02 21:11:05 -0400

    added project detail html

--- src/pages/OneProject.js
+++ src/pages/OneProject.js
@@ -18,22 +18,32 @@ function OneProject({ projects }) {
             <hr></hr>
             <div className="summary-box">
               <h2 id="summary-h2">{p.title}</h2>
-              <p>{p.details}</p>
+              <p><b>Inspiration:</b> {p.inspo}</p>
+              <p><b>Details:</b> {p.details}</p>
+              <p><b>Languages:</b> {p.languages}</p>
 
               {p.deployed_link.length === 0 ?
-              <p id="project-links-p">
-                <a href={p.github_link} target="_blank" rel="noopener noreferrer" className="project-links">GitHub</a> 
+              <p>
+                <b>GitHub: </b>
+                <a href={p.github_link} target="_blank" rel="noopener noreferrer" className="project-links">{p.github_link}</a> 
               </p>
               :
-              <p id="project-links-p">
-                <a href={p.github_link} target="_blank" rel="noopener noreferrer" className="project-links">GitHub</a> | <a href={p.deployed_link} target="_blank" rel="noopener noreferrer" className="project-links">Link</a>
-              </p>
+              <span>
+                <p>
+                  <b>GitHub: </b>
+                  <a href={p.github_link} target="_blank" rel="noopener noreferrer" className="project-links">{p.github_link}</a>
+                </p>
+                <p>
+                  <b>Deployed Link: </b>
+                  <a href={p.deployed_link} target="_blank" rel="noopener noreferrer" className="project-links">{p.deployed_link}</a>
+                </p>
+              </span>
               }
 
             </div>
           </div>
           <div className="bts-column">
-            <h2>Behind The Scenes</h2>
+            <h2>Wireframe</h2>
             <hr></hr>
 
             {p.wireframe_image.length === 0 ?
@@ -48,7 +58,7 @@ function OneProject({ projects }) {
 
           </div>
           <div>
-            <h2>Snapshot</h2>
+            <h2>Demo</h2>
             <hr></hr>
             <div className="snapshot">
               <img src={p.gif_image} alt=""/>
