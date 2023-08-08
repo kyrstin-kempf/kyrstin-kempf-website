4b987ca69592f986710474bedb1b0fc897279f12
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-08-08 13:43:19 -0400

    content edit

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,7 +14,7 @@ function About() {
               My journey into software engineering began in 2014 when I used a drag-and-drop platform to create a website for my mother's, then independent, real estate business. With a specific design in mind, I began dabbling in CSS and HTML to customize the generic theme. Fast forward a couple of years, I got really into the organization of data in spreadsheet software. I soaked up anything I could learn on formulas, conditional formatting, and filters. Over time, I pieced together that software engineering is the beatiful blend of these two elements - creative design and complex data organization. Once I knew this, the idea of pursuing software engineering was an itch that wouldn't go away. So, I joined a bootcamp!
             </p>
             <p>
-              <b>TLDR;</b> I recently completed a software engineering bootcamp, and I am passionate about organizing information through thoughtful design.
+              <b>TL;DR</b> I recently completed a software engineering bootcamp, and I am passionate about organizing information through thoughtful design.
             </p>
           </div>
           <div className="child-three">
